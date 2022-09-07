81483f53f81711839acac850fe13c46aacb0b8d8
Vitaliistf <deosplayt@gmail.com>
2022-09-07 16:45:06 +0300

    1st commit

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-
+        System.out.println();
     }
 
     public static void intializeArray() {
