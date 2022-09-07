74a0cbf647175fc312c139226a8300aeda3d5fe6
Vitaliistf <deosplayt@gmail.com>
2022-09-07 16:57:08 +0300

    1st commit

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        int[] arr = new int[]{1,2,3,4,6,7,8,9};
+        int[] arr = {1,2,3,4,6,7,8,9};
 
 
 
