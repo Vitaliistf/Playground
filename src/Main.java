d6ee6a788860c0f75f2efb8ce0b78a3b751c73c4
Vitaliistf <deosplayt@gmail.com>
2022-09-07 16:42:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+    }
+
+    public static void intializeArray() {
+
+    }
+
+    public static void printArray() {
+
+    }
+
+}
